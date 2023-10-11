08c83639069b5e94060ba4cf1311f6cfda90a2cb
Dhevamuthu <dhevadharshini.m2022cce@sece.ac.in>
2023-10-11 19:52:46 +0530

    hi

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,8 +7,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
-//import Body from './components/body';
-//import ErrorBoundary from './ErrorBoundary';
+import Body from './components/body';
+import Profile from "./components/Profile";
+import SareeDetails from './components/SareeDetails';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,20 +22,39 @@ const appRouter= createBrowserRouter([
     path: "/",
     element: <App/>,
     errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+
+      },
+      
+        {
+          path:'/about',
+          element: <About/>,
+          children:[
+            {
+              path:"profile",
+              element: <Profile />,
+            },
+          ],
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        {
+          path: "/sarees/:sid",
+          element: <SareeDetails />,
+        },
+      ],
   },
-  {
-    path:'/about',
-    element: <About/>,
-  },
-  {
-    path: "contact",
-    element: <Contact />,
-  }
+  
 ]);
 
 
 
-root.render(<RouterProvider router= {appRouter}></RouterProvider>);
+root.render(<RouterProvider router={appRouter}></RouterProvider>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
